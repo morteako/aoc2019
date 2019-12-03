e501b7c8bd70310b51d2361b811b067b4a9af2af
morteako <morteako@student.matnat.uio.no>
2019-12-03 16:10:49 +0100

    fix 3

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,25 +1,18 @@
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE TupleSections #-}
 module Day03 where
 
-import qualified Data.Set as Set
-import Data.Set (Set)
+
 import qualified Data.Map.Strict as Map
 import Data.Map.Strict (Map)
-import Control.Lens
+-- import Control.Lens
 import Data.List
 import Data.List.Split
 import Data.Char
 import Text.Read
 import Data.Monoid
 import Data.Ord
-import Data.Function
 
--- showSet s = case Set.size s of
---     0 -> "."
---     1 -> "*"
---     n -> "X"
 
 newtype Coord = Coord (Int,Int)
     deriving Semigroup  via (Sum Int, Sum Int)
@@ -27,18 +20,14 @@ newtype Coord = Coord (Int,Int)
     deriving Show
     deriving Eq
     deriving Ord
-
+    
 data Dir = U | D | R | L deriving (Show,Read)
 data Path = Path Dir Int  deriving (Show,Read)
 
--- f "U7" = 
+type Paths = ([Path],[Path])
+type Steps = Sum Int
 
--- parse :: String -> Map (Int,Int) (Set Int)
-parse = map parsePaths . lines
-    where
-        parsePaths = map (read @Path . ("Path " ++) . intercalate " " . groupBy f) . splitOn ","
-        f x y = isDigit x && isDigit y || not (isDigit x) && not (isDigit y)
-
+getDir :: Dir -> Coord
 getDir U = Coord (0,1)
 getDir D = Coord (0,-1)
 getDir R = Coord (1,0)
@@ -46,45 +35,55 @@ getDir L = Coord (-1,0)
 
 
 
-exec (Path dir n) pos grid = (last ss, foldr Set.insert grid newSet)
+parse :: String -> Paths
+parse = toTuple . map parsePaths . lines
     where
-        newSet = Set.fromList ss
-        ss = take (n+1) $ iterate (<> getDir dir) pos
+        toTuple [x,y] = (x,y)
+        toTuple _ = error "bad input"
 
-createSet paths = snd $ foldl' f start paths 
+        parsePaths = map (read . ("Path " ++) . intercalate " " . groupBy f) . splitOn ","
+        
+        f x y = isDigit x && isDigit y || not (isDigit x) && not (isDigit y)
+
+
+exec :: Path -> (Coord, Steps) -> Map Coord Steps -> ((Coord, Steps), Map Coord Steps)
+exec (Path dir n) (pos,steps) grid = (last newSteps, Map.unionWith min grid newMap)
+    where
+        newMap = Map.fromList newSteps
+        newSteps = take (n+1) $ iterate (<> (getDir dir, Sum 1)) (pos, steps)
+
+createMap :: [Path] -> Map Coord Steps
+createMap paths = snd $ foldl' f mempty paths 
     where 
-        start = (Coord (0,0), Set.empty)
-        f (pos,grid) p = exec p pos grid
+        f (state,grid) p = exec p state grid
 
-findIntersection (p1, p2)= Set.intersection (createSet p1) (createSet p2)
+findIntersection :: Paths -> Map Coord Steps
+findIntersection (p1, p2) = Map.intersectionWith (+) (createMap p1) (createMap p2)
 
--- manDist (Coord (p1,p2)) (Coord (q1,q2)) = abs (p1-q1) + abs (p2-q2)
+manDist :: Coord -> Int
 manDist (Coord (p1,p2)) = abs p1 + abs p2
 
-solveA = manDist . minimumBy (comparing manDist) . Set.delete mempty . findIntersection 
+calcIntersections :: Paths -> Map Coord Steps
+calcIntersections = Map.delete (Coord (0,0)) . findIntersection
 
+solveA :: Paths -> Int
+solveA = manDist . minimumBy (comparing manDist) . Map.keys . calcIntersections
 
-solveB = undefined
+solveB :: Paths -> Int
+solveB = getSum . minimum . calcIntersections
 
 
         
 main :: IO ()
 main = do
     contents <- readFile "data/day03.txt"
-    -- let contents = "R8,U5,L5,D3\nU7,R6,D4,L4"
-    -- let contents = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"
+
     let parsed = parse contents
 
-    let [p1,p2] = parsed
+    print $ solveA parsed
+    print $ solveB parsed
     
-    -- print $ createSet p1
-    -- print $ createSet p2
-    print $ solveA (p1, p2)
-    -- print $ exec (Path U 5) (Coord (1,1)) Set.empty
-    -- print $ solveA parsed
-    -- print $ solveB parsed
-    
-    return ()
+
     
     
 
