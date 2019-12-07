fcc1cc3fcde1acfb30589170cff48573eaa926d6
morteako <morteako@student.matnat.uio.no>
2019-12-07 11:19:13 +0100

    added reader + fixes

--- src/Day05.hs
+++ src/Day05.hs
@@ -8,82 +8,74 @@ import Data.Vector (Vector, fromList ,(!),(//))
 import           Data.List.Split                ( splitOn )
 import           Safe                           (headMay)
 import Data.Strings
-import Debug.Trace
+import Control.Monad.Reader
 
-parse :: String -> (Vector Int, [Int])
-parse xs = (fromList res, res)
-  where res = fmap read . splitOn "," $ xs
+parse :: String -> Vector Int
+parse = fromList . fmap read . splitOn ","
 
 data Mode = Pos | Inter deriving Show
 
+getVal :: Mode -> Vector Int -> Int -> Int
 getVal Inter  _ val = val
 getVal Pos   ar val = ar ! val
 
-
+toMode :: Char -> Mode
 toMode '0' = Pos
 toMode '1' = Inter
 toMode x = error $ "toMode " ++ show x
 
-
-shit (show -> code) = (toMode a, toMode b, toMode c, read op :: Int)
+parseCode :: Int -> (Mode, Mode, Mode, Int)
+parseCode (show -> code) = (toMode a, toMode b, toMode c, read op :: Int)
     where
-      --ORDER!?
       (c:b:a:op) = strPadLeft '0' 5 code
 
-input = 1
--- input = undefined
-
-interpretPos :: Int -> Vector Int -> [Int]
--- interpretPos pos ar = []
+interpretPos :: Int -> Vector Int -> Reader Int [Int]
 interpretPos pos ar
-  -- | traceShow (length ar, ar ! pos, ar ! pos + 1, "pos", pos, "op", ar ! pos, shit $ ar ! pos) False = undefined
+  | 99 <- ar ! pos = return []
 
-  | 99 <- ar ! pos = []
-
-  | (am,_,_,3) <- shit $ ar ! pos
+  | (am,_,_,3) <- parseCode $ ar ! pos
   , inputPos <- ar ! (pos+1)  
-  = interpretPos (pos+2) (set (ix inputPos) 1 ar)
-
+  = do
+      input <- ask
+      interpretPos (pos+2) (set (ix inputPos) input ar)
   
-  | (am,_,_,4) <- shit $ ar ! pos 
+  | (am,_,_,4) <- parseCode $ ar ! pos 
   , v <- ar ! (pos+1)
-  = getVal am ar v : interpretPos (pos+2) ar
+  = (getVal am ar v:) <$> interpretPos (pos+2) ar
 
-
-
-
-  | (am,bm,cm,op) <- shit $ ar ! pos
+  | (am,bm,cm,op) <- parseCode $ ar ! pos
   , [a,b,stored] <- fmap (ar !) [pos+1..pos+3]
   = case op of 
       1 -> interpretPos (pos+4) (doOp (+) ar (getVal am ar a) (getVal bm ar b) stored) 
-      2 -> interpretPos (pos+4) (doOp (*) ar (getVal am ar a) (getVal bm ar b) stored) 
-      _ -> error $ show (pos,Vector.take 15 ar,op)
+      2 -> interpretPos (pos+4) (doOp (*) ar (getVal am ar a) (getVal bm ar b) stored)
+      5 -> if getVal am ar a /= 0 then interpretPos (getVal bm ar b) ar else interpretPos (pos+3) ar
+      6 -> if getVal am ar a == 0 then interpretPos (getVal bm ar b) ar else interpretPos (pos+3) ar
+      7 -> interpretPos (pos+4) $ store (<) (getVal am ar a) (getVal bm ar b) stored ar
+      8 -> interpretPos (pos+4) $ store (==) (getVal am ar a) (getVal bm ar b) stored ar
+      _ -> error "?"
   | otherwise = 
     error "opsie"
+
+store :: (Int -> Int -> Bool) -> Int -> Int -> Int -> Vector Int -> Vector Int
+store p a b c ar = if p a b then set (ix c) 1 ar else set (ix c) 0 ar 
 
 doOp :: (Int -> Int -> Int) -> Vector Int -> Int -> Int -> Int -> Vector Int
 doOp (#) ar a b (ix -> lens)  
     = set lens (a # b) ar
 
 
-solveA :: Vector Int -> [Int] -> [Int]
-solveA ar list = interpretPos 0 ar
+solveA :: Vector Int -> [Int]
+solveA = flip runReader 1 . interpretPos 0
 
+solveB :: Vector Int -> [Int]
+solveB = flip runReader 5 . interpretPos 0
 
--- solveB :: Vector Int -> [Int] -> Maybe Int
--- solveB ar list = fmap calc $ headMay $ filter (checkTarget . f) ((,) <$> [1..1000] <*> [1..1000])
---     where
---         f (one,two) =  (! 0) <$> interpret list (ar // [(1,one),(2,two)])
---         checkTarget x = Just 19690720 == x
---         calc (x,y) = 100*x + y
 
 main :: IO ()
 main = do
   contents <- readFile "data/day05.txt"
-  let (ar,list) = parse contents
+  let ar = parse contents
 
-
-
-  print $ solveA ar list
-  -- print $ solveA ar list
-  -- print $ solveB ar list
+  print $ solveA ar 
+  print $ solveB ar 
+  
